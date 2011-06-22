b413523f701fdf2a4b329cb9c99fc74f4c4a21c5
Guillaume LO RE <lowreg@gmail.com>
2011-06-22 12:31:37 +0200

    Added options to binding

--- JsBeautifier/beautifier.py
+++ JsBeautifier/beautifier.py
@@ -7,20 +7,22 @@
 # selected, the whole file will be beautified.  
 #
 # Key binding example 
-# { "keys": ["alt+shift+b"], "command": "beau" }
+# { "keys": ["alt+shift+b"], "command": "beau", "args": {"indent_size": 4, "brace_style": "collapse"} }
+# You can change the indent_size and brace_style according to jsbeautifier options.
 
 import sublime, sublime_plugin, jsbeautifier
 
 class BeauCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, indent_size, brace_style):
         selections = self.view.sel()
         for sel in selections:
             if sel.size() == 0 and len(selections) == 1:
                 region = sublime.Region(0, self.view.size())
             else:
-                region = sublime.Region(sel.begin(), sel.end())
+                region = sel
             content = self.view.substr(region)
             opts = jsbeautifier.default_options()
-            opts.brace_style = 'expand'
+            opts.brace_style = brace_style
+            opts.indent_size = int(indent_size)
             res = jsbeautifier.beautify(content, opts)
             self.view.replace(edit, region, res)
